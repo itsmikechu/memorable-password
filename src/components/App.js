c5bad284ba1e653a08809ee02cb04ee9d486d1fe
Mike Chu <mchu@ashworthcollege.edu>
2017-05-21 15:04:28 -0600

    App: set phrase-to-password algorithm

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,33 @@ import './App.css';
 
 class App extends Component {
   computePassword = () => {
-    console.log("Going to compute password");
+    let passwordCharacters = [];
+
+    // split each word
+    let wordArray = phrase.split(' ');
+
+    let nonAlphaRegex = /[^A-Za-z]/i;
+    for (let word of wordArray) {
+
+      // see if there are non-alpha chars in the current word
+      const nonAlphaMatches = word.match(nonAlphaRegex);
+      // if there are non-alpha characters within this word
+      if (nonAlphaMatches) {
+        // split the word up into characters
+        const wordCharacters = word.split('');
+        // push the first char regardless of type
+        passwordCharacters.push(wordCharacters[0]);
+        // shift all the elements to the left to remove the first char
+        wordCharacters.shift();
+        // extract the remaining non-alpha chars from the array
+        for (let blah of wordCharacters) {
+          if (nonAlphaRegex.test(blah)) {
+            passwordCharacters.push(blah);
+          }
+        }
+      } else {
+        passwordCharacters.push(word.charAt(0));
+      }
   }
 
   render() {
