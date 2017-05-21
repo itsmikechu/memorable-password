d1573f291336d5e7f982c68144c09ca0f1ba617f
Mike Chu <mchu@ashworthcollege.edu>
2017-05-20 18:00:41 -0600

    Project: adjusting project file locations

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../images/logo.svg'
 import './App.css';
 
 class App extends Component {
@@ -10,9 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Memorable Password</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
