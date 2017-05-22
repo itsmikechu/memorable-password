6bbfba91284aa141f9d9403055b464345ca7eef7
Mike Chu <mchu@ashworthcollege.edu>
2017-05-22 08:26:21 -0600

    Password: copy functionality is working

--- src/components/Password.js
+++ src/components/Password.js
@@ -3,14 +3,53 @@ import './Password.css'
 
 class Password extends Component {
     copyPassword = () => {
-        console.log(this.props.passwordResult);
+        // make contenteditable
+        this.passwordResult.setAttribute('contenteditable', 'true');
+
+        // focus the field
+        this.passwordResult.focus();
+
+        // get the range
+        var range = document.createRange();
+        range.selectNodeContents(this.passwordResult);
+
+        // get the selection
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        // try to copy the selection
+        let copiedSuccessfully;
+        try {
+            copiedSuccessfully = document.execCommand('copy');
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+
+        // confirm to UI it's done
+        let resultIconClass = 'fa-exclamation-triangle';
+        if (copiedSuccessfully) {
+            resultIconClass = 'fa-check';
+        }
+        this.copyButton.classList.remove('fa-copy');
+        this.copyButton.classList.add(resultIconClass);
+        window.setTimeout(() => {
+            this.copyButton.classList.remove('fa-exclamation-triangle');
+            this.copyButton.classList.remove('fa-check');
+            this.copyButton.classList.add('fa-copy');
+        }, 2000);
+
+        // remove focus, selection, and contenteditable
+        sel.removeAllRanges();
+        this.passwordResult.blur();
+        this.passwordResult.removeAttribute('contenteditable');
     }
 
     render() {
         return (
             <div className="Password">
-                <h3>{this.props.passwordResult}</h3>
-                <i className="fa fa-copy" onClick={this.copyPassword} title="Copy Password"></i>
+                <h3 ref={(h3) => { this.passwordResult = h3; }}>{this.props.passwordResult}</h3>
+                <i ref={(i) => { this.copyButton = i; }} className="fa fa-copy copyButton" onClick={this.copyPassword} title="Copy Password" ></i>
             </div>
         )
     }
