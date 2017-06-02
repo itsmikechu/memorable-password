cb4639b2825257cb3674ac19110afc39d560bdd0
Mike Chu <mchu@ashworthcollege.edu>
2017-06-01 23:23:30 -0600

    Footer: added a bit of usage information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="Footer">
+                <div>Once this page is loaded, you can disconnect from the Internet while you create passwords.</div>
                 <a href="https://github.com/itsmikechu/memorable-password" target="repo">
                     <i className="fa fa-github fa-2x"></i>
                 </a>
