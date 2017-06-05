a46c08f133c6903d5027e60ba412d0a9d44a2190
Mike Chu <mchu@ashworthcollege.edu>
2017-06-05 07:24:36 -0600

    Analytics: added GA and footer note

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ class Footer extends Component {
         return (
             <div className="Footer">
                 <div>Once this page is loaded, you can disconnect from the Internet while you create passwords.</div>
+                <div>Google Analytics in use for the purpose of seeing how useful this site is. Feel free to block it.</div>
                 <a href="https://github.com/itsmikechu/memorable-password" target="repo">
                     <i className="fa fa-github fa-2x"></i>
                 </a>
