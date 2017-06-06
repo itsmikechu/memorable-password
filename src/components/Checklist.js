e18531c8239992fb3ce65b1e966fb6ec6a4f741b
Mike Chu <mchu@ashworthcollege.edu>
2017-06-05 23:54:34 -0600

    Checklist: TDDed count functions; I <3 TDD

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import './Checklist.css'
 
 class Checklist extends Component {
+    runRegexToGetLength = (value, regex) => {
+        if (value === null || value.match(regex) === null) {
+            return 0;
+        }
+        return value.match(regex).length;
+    }
+
+    countLowercase = (value) => {
+        return this.runRegexToGetLength(value, /[a-z]/g);
+    }
+
+    countUppercase = (value) => {
+        return this.runRegexToGetLength(value, /[A-Z]/g);
+    }
+
+    countNumbers = (value) => {
+        return this.runRegexToGetLength(value, /[0-9]/g);
+    }
+
+    countSpecialCharacters = (value) => {
+        return this.runRegexToGetLength(value, /\W/g);
+    }
+
+    satisfiesMinimumLength = (value, minimumLength) => {
+        return (value === null ? false : value.length >= minimumLength);
+    }
+    
     render() {
         return (
             <ul className="Checklist">
--- src/components/Checklist.test.js
+++ src/components/Checklist.test.js
@@ -2,27 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Checklist from './Checklist';
 
+const cl = new Checklist();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Checklist />, div);
 });
 
 it('finds lowercase characters', () => {
-  throw 'Not implemented' ;
+  const positiveCase = 'Fe53TvF*#0)f';
+  const negativeCase = 'SDF#MC0329!EL';
+
+  expect(cl.countLowercase(positiveCase)).toBeGreaterThan(0);
+  expect(cl.countLowercase(negativeCase)).toBe(0);
 });
 
 it('finds UPPERCASE characters', () => {
-  throw 'Not implemented';
+  const positiveCase = 'QsLR$0(vVe48sLV';
+  const negativeCase = 'fkd@e40nml1ee';
+
+  expect(cl.countUppercase(positiveCase)).toBeGreaterThan(0);
+  expect(cl.countUppercase(negativeCase)).toBe(0);
 });
 
 it('finds numb3r5 in the password', () => {
-  throw 'Not implemented';
+  const positiveCase = 'f#58FSmvRk)3OcVzZ';
+  const negativeCase = 'dlkWEVzQAsEp[MF';
+
+  expect(cl.countNumbers(positiveCase)).toBeGreaterThan(0);
+  expect(cl.countNumbers(negativeCase)).toBe(0);
 });
 
 it('finds specia! ch@racters', () => {
-  throw 'Not implemented';
+  const positiveCase = 'D4bN43V(c@fe^fH';
+  const negativeCase = 'dlfk3CS89SDFk30';
+
+  expect(cl.countSpecialCharacters(positiveCase)).toBeGreaterThan(0);
+  expect(cl.countSpecialCharacters(negativeCase)).toBe(0);
+});
+
+
+it('handles empty passwords ok', () => {
+  expect(cl.countLowercase('')).toBe(0);
+  expect(cl.countUppercase('')).toBe(0);
+  expect(cl.countNumbers('')).toBe(0);
+  expect(cl.countSpecialCharacters('')).toBe(0);
 });
 
 it('determines that computed password length >= 8', () => {
-  throw 'Not implemented';
+  const minimumPasswordLength = 8;
+  const positiveCase = 'Fe#6j)vdL84@';
+  const negativeCase = 'pR$c#9J';
+
+  expect(cl.satisfiesMinimumLength(positiveCase, minimumPasswordLength)).toBe(true);
+  expect(cl.satisfiesMinimumLength(negativeCase,minimumPasswordLength)).toBe(false);
 });
