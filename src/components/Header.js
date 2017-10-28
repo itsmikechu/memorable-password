91f6ff925a14bf47ff27088f36e1d2e58ceedba0
Mike Chu <mike@chu.ms>
2017-10-28 14:34:07 -0600

    Added header back and formatted it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg'
 import './Header.css'
 
 class Header extends Component {
     render() {
         return (
             <div className="Header">
-                <img src={logo} className="logo" alt="logo" />
                 <h1>Memorable Password</h1>
                 <h3>Create a more-secure, memorable password.</h3>
             </div>
         )
     }
-} 
+}
 
 export default Header;
